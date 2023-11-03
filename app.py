c8c7945600bb3202af57936de6c75b5e2faf11a0
jb_frhl <72656353+sirthirrygolooo@users.noreply.github.com>
2023-11-03 14:54:26 +0100

    y'a du neuf

--- app.py
+++ app.py
@@ -1,131 +1,175 @@
-#! /usr/bin/python
-# -*- coding:utf-8 -*-
-from flask import Flask, request, render_template, redirect, url_for, abort, flash
+from flask import Flask, request, render_template, redirect, flash
 
 app = Flask(__name__)
-app.secret_key = 'secrettttt'
+app.secret_key = 'secretkeyhehe'
 
-articles = [
-    {'id': 1, 'nom': 'Enveloppes (50p)', 'type_article_id': 1, 'prix': 2, 'stock': 1, 'description': None, 'image': None},
-    {'id': 2, 'nom': 'Stylo noir', 'type_article_id': 1, 'prix': 1, 'stock': 10, 'description': None, 'image': 'stylo.jpeg'},
-    {'id': 3, 'nom': 'Boite de rangement', 'type_article_id': 1, 'prix': 3, 'stock': 7, 'description': None, 'image': 'boites.jpeg'},
-    {'id': 4, 'nom': 'Chaise', 'type_article_id': 2, 'prix': 40, 'stock': 2, 'description': None, 'image': 'chaise.jpeg'},
-    {'id': 5, 'nom': 'Tables', 'type_article_id': 2, 'prix': 200, 'stock': 3, 'description': None, 'image': 'table.jpeg'},
-    {'id': 6, 'nom': 'Salon de Jardin alu', 'type_article_id': 3, 'prix': 149, 'stock': 3, 'description': None, 'image': 'salonJardin2.jpg'},
-    {'id': 7, 'nom': 'Table+6 fauteuilles de Jardin', 'type_article_id': 3, 'prix': 790, 'stock': 3, 'description': None, 'image': 'tableFauteuilsJardin1.jpg'},
-    {'id': 8, 'nom': 'Set Table + 4 bancs', 'type_article_id': 3, 'prix': 229, 'stock': 2, 'description': None, 'image': 'setTableChaises.jpg'},
-    {'id': 9, 'nom': 'arrosoir bleu', 'type_article_id': 4, 'prix': 14, 'stock': 2, 'description': None, 'image': 'arrosoir1.jpg'},
-    {'id': 10, 'nom': 'arrosoir griotte', 'type_article_id': 4, 'prix': 10, 'stock': 2, 'description': None, 'image': 'arrosoir2.jpg'},
-    {'id': 11, 'nom': 'tuyau arrosage', 'type_article_id': 4, 'prix': 32, 'stock': 3, 'description': None, 'image': 'tuyauArrosage1.jpg'},
-    {'id': 12, 'nom': 'tournevis', 'type_article_id': 5, 'prix': 24, 'stock': 3, 'description': None, 'image': 'lotTourneVis.jpg'},
-    {'id': 13, 'nom': 'marteau menuisier', 'type_article_id': 5, 'prix': 8, 'stock': 3, 'description': None, 'image': 'marteau.jpg'},
-    {'id': 14, 'nom': 'pince multiprise', 'type_article_id': 5, 'prix': 22, 'stock': 3, 'description': None, 'image': 'pinceMultiprise.jpg'},
-    {'id': 15, 'nom': 'perceuse', 'type_article_id': 5, 'prix': 150, 'stock': 3, 'description': None, 'image': 'perceuse.jpg'}
+marques = [
+ {'id' : 1, 'libelle':'Yamaha', 'logo':'logo-yamaha.jpg'},
+ {'id' : 2, 'libelle':'Honda', 'logo':'logo-Honda.png'},
+ {'id' : 3, 'libelle':'Kawasaki','logo':'logo-kawasaki.jpg'},
+ {'id' : 4, 'libelle':'BMW', 'logo':'logo-bmw.png'},
+ {'id' : 5, 'libelle':'Suzuki', 'logo':'logo-Suzuki.png'},
+ {'id' : 6, 'libelle':'Triumph','logo':'logo-Triumph.jpg'},
+ {'id' : 7, 'libelle':'Piaggio','logo':'logo-Piaggio.png'},
+ {'id' : 8, 'libelle':'KTM', 'logo':'logo-KTM.png'},
+ {'id' : 9, 'libelle':'Ducati', 'logo':'logo-Ducati.png'},
+ {'id' : 10, 'libelle':'Harley Davidson', 'logo':'logo-Harley-Davidson.png'},
+ {'id' : 11, 'libelle':'Kymco', 'logo':'logo-kymco.jpg'},
+ {'id' : 12, 'libelle':'Aprilia','logo':'logo-aprilia.jpg'}
 ]
 
-types_articles = [
-    {'id': 1, 'libelle': 'Fourniture de bureau'},
-    {'id': 2, 'libelle': 'Mobilier'},
-    {'id': 3, 'libelle': 'Mobilier Jardin'},
-    {'id': 4, 'libelle': 'Arrosage'},
-    {'id': 5, 'libelle': 'Outils'},
-    {'id': 6, 'libelle': 'Divers'}
+motos = [
+ {'id' : 1, 'nomMoto':'T-MAX', 'puissance':400, 'DateMiseEnCirculation':'2012-06-15', 'couleur':'black', 'marque_id':1, 'photo':'T-MAX-noir.jpg'},
+ {'id' : 2, 'nomMoto':'Niken', 'puissance':350, 'DateMiseEnCirculation':'2007-06-15', 'couleur':'yellow', 'marque_id':1, 'photo':'niken.jpg'},
+ {'id' : 3, 'nomMoto':'MT09', 'puissance':250, 'DateMiseEnCirculation':'2015-06-15', 'couleur':'red', 'marque_id':1, 'photo':'mt09.jpg'},
+ {'id' : 4, 'nomMoto':'X-MAX', 'puissance':450, 'DateMiseEnCirculation':'2019-06-15', 'couleur':'orange', 'marque_id':1, 'photo':'xmax.jpg'},
+ {'id' : 5, 'nomMoto':'MT09-Tracer', 'puissance':100, 'DateMiseEnCirculation':'2001-06-15', 'couleur':'purple', 'marque_id':1, 'photo':'mt09tracer.jpg'},
+ {'id' : 6, 'nomMoto':'Africa Twin', 'puissance':500, 'DateMiseEnCirculation':'1999-07-21', 'couleur':'white', 'marque_id':2, 'photo':'AFRICA_TWIN.jpg'},
+ {'id' : 7, 'nomMoto':'Pan European', 'puissance':1500, 'DateMiseEnCirculation':'2000-09-30', 'couleur':'black', 'marque_id':2, 'photo':'panEuropean.jpg'},
+ {'id' : 8, 'nomMoto':'Sh','puissance':150, 'DateMiseEnCirculation':'2020-09-30', 'couleur':'black', 'marque_id':2, 'photo':'sh.jpg'},
+ {'id' : 8, 'nomMoto':'Swing', 'puissance':200, 'DateMiseEnCirculation':'2021-09-30', 'couleur':'black', 'marque_id':2, 'photo':'swing.jpg'},
+ {'id' : 10, 'nomMoto':'PS','puissance':6, 'DateMiseEnCirculation':'2017-09-30', 'couleur':'black', 'marque_id':2, 'photo':'ps.jpeg'},
+ {'id' : 11, 'nomMoto':'Deauville', 'puissance':100, 'DateMiseEnCirculation':'2010-09-30', 'couleur':'black', 'marque_id':2, 'photo':'deauville.jpg'},
+ {'id' : 12, 'nomMoto':'A1','puissance':100, 'DateMiseEnCirculation':'2025-08-27', 'couleur':'green', 'marque_id':3, 'photo':'a1.jpg'},
+ {'id' : 13, 'nomMoto':'H2','puissance':1000, 'DateMiseEnCirculation':'2020-08-27', 'couleur':'green', 'marque_id':3, 'photo':'h2.jpg'},
+ {'id' : 14, 'nomMoto':'VN','puissance':500, 'DateMiseEnCirculation':'2024-08-27', 'couleur':'green', 'marque_id':3, 'photo':'vn.jpg'},
+ {'id' : 15, 'nomMoto':'Z', 'puissance':200, 'DateMiseEnCirculation':'2023-08-27', 'couleur':'green', 'marque_id':3, 'photo':'z.jpg'},
+ {'id' : 16, 'nomMoto':'Ninja 400', 'puissance':400, 'DateMiseEnCirculation':'2022-08-27', 'couleur':'green', 'marque_id':3, 'photo':'ninja400.jpg'},
+ {'id' : 17, 'nomMoto':'Vulcan 650', 'puissance':650, 'DateMiseEnCirculation':'2021-08-27', 'couleur':'green', 'marque_id':3, 'photo':'vulcan250.jpg'},
+ {'id' : 18, 'nomMoto':'K 1600', 'puissance':1600, 'DateMiseEnCirculation':'2010-08-27', 'couleur':'blue', 'marque_id':4, 'photo':'k1600.jpg'},
+ {'id' : 19, 'nomMoto':'F 900 XR', 'puissance':900, 'DateMiseEnCirculation':'2011-08-27', 'couleur':'blue', 'marque_id':4, 'photo':'f900xr.jpg'},
+ {'id' : 20, 'nomMoto':'R 1250 R', 'puissance':1250, 'DateMiseEnCirculation':'2012-08-27', 'couleur':'blue', 'marque_id':4, 'photo':'r1250r.jpeg'},
+ {'id' : 21, 'nomMoto':'R 1200 RT', 'puissance':1200, 'DateMiseEnCirculation':'2013-08-27', 'couleur':'blue', 'marque_id':4, 'photo':'r1200rt.jpg'},
+ {'id' : 22, 'nomMoto':'R 900 RT', 'puissance':900, 'DateMiseEnCirculation':'2014-08-27', 'couleur':'blue', 'marque_id':4, 'photo':'r900rt.jpg'},
+ {'id' : 23, 'nomMoto':'S 1000 SR', 'puissance':1000, 'DateMiseEnCirculation':'2015-08-27', 'couleur':'blue', 'marque_id':4, 'photo':'s1000sr.jpeg'}
 ]
 
 @app.route('/')
-def show_accueil():
-    return render_template('layout.html')
+def show_layout():
+    return render_template('layouteu.html')
 
-@app.route('/type-article/show')
-def show_type_article():
-    return render_template('type_article/show_type_article.html', types_articles=types_articles)
+@app.route('/marque/show')
+def show_marque():
+    return render_template('brand/show_marque.html', brand = marques)
 
-@app.route('/type-article/add', methods=['GET'])
-def add_type_article():
-    return render_template('type_article/add_type_article.html')
+@app.route('/marque/add', methods=['GET'])
+def add_marque():
+    return render_template('brand/add_marque.html')
 
-@app.route('/type-article/add', methods=['POST'])
-def valid_add_type_article():
+@app.route('/marque/add', methods=['POST'])
+def valid_add_marque():
     libelle = request.form.get('libelle', '')
-    print(u'type ajouté , libellé :', libelle)
-    message = u'type ajouté , libellé :'+libelle
+    print(u'marque ajoutée , libellé : ', libelle)
+    message = u'marque ajoutée, libellé : '+libelle
     flash(message, 'alert-success')
-    return redirect('/type-article/show')
+    return redirect('/marque/show')
 
-@app.route('/type-article/delete', methods=['GET'])
-def delete_type_article():
+@app.route('/marque/delete', methods=['GET'])
+def delete_marque():
     id = request.args.get('id', '')
-    print ("un type d'article supprimé, id :",id)
-    message=u'un type d\'article supprimé, id : ' + id
+    print ("une marque supprimée, id : ",id)
+    message=u'une marque supprimée, id : ' + id
     flash(message, 'alert-warning')
-    return redirect('/type-article/show')
+    return redirect('/marque/show')
 
-@app.route('/type-article/edit', methods=['GET'])
-def edit_type_article():
+
+@app.route('/marque/edit', methods=['GET'])
+def edit_marque():
     id = request.args.get('id', '')
-    libelle = request.args.get('libelle', '')     # comment passé plusieurs paramètres (clé primaire composés)
     id=int(id)
-    type_article = types_articles[id-1]
-    return render_template('type_article/edit_type_article.html', type_article=type_article)
+    marque = marques[id-1]
+    return render_template('brand/edit_marque.html', marque=marque)
 
-@app.route('/type-article/edit', methods=['POST'])
-def valid_edit_type_article():
+@app.route('/marque/edit', methods=['POST'])
+def valid_edit_marque():
     libelle = request.form['libelle']
     id = request.form.get('id', '')
-    print(u'type article modifié, id: ',id, " libelle :", libelle)
-    message=u'type article modifié, id: ' + id + " libelle : " + libelle
+    print(u'marque modifiée, id: ',id, " libelle :", libelle)
+    message=u'une marque modifiée, id: ' + id + " libelle : " + libelle
     flash(message, 'alert-success')
-    return redirect('/type-article/show')
+    return redirect('/marque/show')
 
-@app.route('/article/show')
-def show_article():
-    # print(articles)
-    return render_template('article/show_article.html', articles=articles)
+@app.route('/moto/show')
+def show_moto():
+    return render_template('moto/show_moto.html', moto=motos, brand=marques)
 
-@app.route('/article/add', methods=['GET'])
-def add_article():
-    return render_template('article/add_article.html', types_articles=types_articles)
+@app.route('/moto/add', methods=['GET'])
+def add_moto():
+    return render_template('moto/add_moto.html', brand=marques, moto=motos)
 
-@app.route('/article/add', methods=['POST'])
-def valid_add_article():
+@app.route('/moto/add', methods=['POST'])
+def valid_add_moto():
     nom = request.form.get('nom', '')
-    type_article_id = request.form.get('type_article_id', '')
-    prix = request.form.get('prix', '')
-    stock = request.form.get('stock', '')
-    description = request.form.get('description', '')
-    image = request.form.get('image', '')
-    print(u'article ajouté , nom: ', nom, ' - type_article_id :', type_article_id, ' - prix:', prix, ' - stock:', stock, ' - description:', description, ' - image:', image)
-    message = u'article ajouté , nom:'+nom + '- type_article_id :' + type_article_id + ' - prix:' + prix + ' - stock:'+  stock + ' - description:' + description + ' - image:' + image
+    brand_id = request.form.get('marque_id', '')
+    puissance = request.form.get('puissance', '')
+    couleur = request.form.get('couleur', '')
+    miseEnCirculation = request.form.get('miseEnCirculation', '')
+    photo = request.form.get('photo', '')
+    print(u'Nouvelle moto , nom : ', nom, ' | id marque : ', brand_id, ' | Puissance : ', puissance, ' | couleur : ', couleur, ' | Mise en circulation : ', miseEnCirculation, ' | photo : ', photo)
+    message = u'Nouvelle moto , nom : '+nom + ' | id marque : ' + brand_id + ' | Puissance : ' + puissance + ' | couleur : '+  couleur + ' | Mise en circulation : ' + miseEnCirculation + ' | photo : ' + photo
     flash(message, 'alert-success')
-    return redirect('/article/show')
+    return redirect('/moto/show')
 
-@app.route('/article/delete', methods=['GET'])
-def delete_article():
+@app.route('/moto/delete', methods=['GET'])
+def delete_moto():
     id = request.args.get('id', '')
+    print ("une moto supprimée, id : ",id)
     message=u'un article supprimé, id : ' + id
     flash(message, 'alert-warning')
-    return redirect('/article/show')
+    return redirect('/moto/show')
 
-@app.route('/article/edit', methods=['GET'])
-def edit_article():
+@app.route('/moto/edit', methods=['GET'])
+def edit_moto():
     id = request.args.get('id', '')
     id=int(id)
-    article = articles[id-1]
-    return render_template('article/edit_article.html', article=article, types_articles=types_articles)
+    moto1 = motos[id-1]
+    return render_template('moto/edit_moto.html', moto=moto1, brand=marques)
 
-@app.route('/article/edit', methods=['POST'])
-def valid_edit_article():
+@app.route('/moto/edit', methods=['POST'])
+def valid_edit_moto():
     id = request.form.get('id', '')
     nom = request.form.get('nom', '')
-    type_article_id = request.form.get('type_article_id')
-    prix = request.form.get('prix', '')
-    stock = request.form.get('stock', '')
-    description = request.form.get('description', '')
-    image = request.form.get('image', '')
-    print(u'article modifié , nom : ', nom, ' - type_article_id :', type_article_id, ' - prix:', prix, ' - stock:', stock, ' - description:', description, ' - image:', image)
-    message = u'article modifié , nom:'+nom + '- type_article_id :' + str(type_article_id) + ' - prix:' + prix + ' - stock:'+  stock + ' - description:' + description + ' - image:' + image
+    brand_id = request.form.get('marque_id')
+    puissance = request.form.get('puissance', '')
+    miseEnCirculation = request.form.get('miseEnCirculation', '')
+    couleur = request.form.get('couleur', '')
+    photo = request.form.get('photo', '')
+    print(u'moto modifiée , nom : ', nom, ' | brand_id :', brand_id, ' | puissance:', puissance, ' | mise en circulation:', miseEnCirculation, ' | couleur :', couleur, ' | image:', photo)
+    message = u'moto modifiée , nom:'+nom + ' | brand_id :' + str(brand_id) + ' | puissance:' + puissance + ' | mise en circulation:'+  miseEnCirculation + ' | couleur:' + couleur + ' | image:' + photo
     flash(message, 'alert-success')
-    return redirect('/article/show')
+    return redirect('/moto/show')
+
+@app.route('/moto/filtre', methods=['GET'])
+def filtre_moto():
+    print("filtre")
+    filter_word= request.args.get('filter_word', None)
+    filter_value_min = request.args.get('filter_value_min', None)
+    filter_value_max = request.args.get('filter_value_max', None)
+    filter_items = request.args.getlist('filter_items', None)
+    if filter_word and filter_word != "":
+        message=u'filtre sur le mot  : '+filter_word
+        flash(message, 'alert-success')
+    if filter_value_min or filter_value_max :
+        if filter_value_min.isdecimal() and filter_value_max.isdecimal():
+            if int(filter_value_min) < int(filter_value_max):
+                message=u'filtre sur la colonne avec un numérique entre : '+filter_value_min+' et '+filter_value_max
+                flash(message, 'alert-success')
+            else :
+                message=u'min < max'
+                flash(message, 'alert-warning')
+        else :
+            message=u'min et max doivent être des numériques'
+            flash(message, 'alert-warning')
+    if filter_items and filter_items != []:
+        message=u'case à cocher selectionnée : '
+        for case in filter_items :
+            message+= 'id : '+case+' '
+        flash(message, 'alert-success')
+    return render_template('/moto/filtre_moto.html', moto=motos, brand=marques)
+        
+
 
 if __name__ == '__main__':
     app.run()
+
+
